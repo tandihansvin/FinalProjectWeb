9cde329a0b337d4012c7078bd83d60496e2b94ce
〔≡◉︵◉≡〕 <anakaiti@users.noreply.github.com>
2018-03-30 13:38:50 +0700

    Implement TransactionController@getLastStatus

--- app/Http/Controllers/TransactionController.php
+++ app/Http/Controllers/TransactionController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\TransactionHeader;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class TransactionController extends Controller
 {
@@ -15,8 +16,8 @@ class TransactionController extends Controller
     public function getLastStatus(){
 //        return auth('api')->user()->id;
         $headTrans = TransactionHeader::where('user_id',auth('api')->user()->id)->get();
-        foreach ($headTrans as $trans){
-            $trans->statusChange->latest('time')->first()->status;
+        foreach ($headTrans as &$trans){
+            $trans['status'] = $trans->statusChange()->latest('time')->first()->status->name;
         }
         return $headTrans;
     }
